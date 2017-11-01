8741b5932b722b280a4c7b9ed9fe5b2ef79df63c
Yuta Hinokuma <yuta@abeja.asia>
2017-11-01 11:09:36 +0000

    temprary commit

--- spec/rspec/core/action_check_helpers_spec.rb
+++ spec/rspec/core/action_check_helpers_spec.rb
@@ -8,6 +8,7 @@ module RSpec::Core
       @rikka_is = nil
     end
 
+=begin
     actions 'first action' do
       action 'set nadeko is cute' do
         @nadeko_is = 'cute'
@@ -29,7 +30,37 @@ module RSpec::Core
 
     actions 'empty actions' do
     end
+=end
 
+    actions 'branch' do
+      action 'set nadeko is cute' do
+        @nadeko_is = 'cute'
+      end
+
+      branch 'nest1' do
+        check 'nadeko_is = "cute"' do
+          expect(@nadeko_is).to eq 'cute'
+        end
+
+        action 'set rikka is cute' do
+          @rikka_is = 'cute'
+        end
+
+        check 'nadeko_is = "cute" and rikka_is = "cute"' do
+          expect(@nadeko_is).to eq 'cute'
+          expect(@rikka_is).to eq 'cute'
+        end
+      end
+
+      branch 'nest2' do
+        check 'nadeko_is = "cute" and rikka_is is nil' do
+          expect(@nadeko_is).to eq 'cute'
+          expect(@rikka_is).to be_nil
+        end
+      end
+    end
+
+=begin
     actions 'only context' do
       action 'set rikka is cute' do
         @rikka_is = 'cute'
@@ -62,5 +93,6 @@ module RSpec::Core
       end
 
     end
+=end
   end
 end
