d1c26f08b7ff463cae33324986e73d56982ee737
Yuta Hinokuma <yuta@abeja.asia>
2017-11-02 01:48:08 +0000

    implemented branch

--- lib/rspec/core/action_check_helpers.rb
+++ lib/rspec/core/action_check_helpers.rb
@@ -3,9 +3,6 @@ module RSpec
   module Core
     module ActionCheckHelpers
       module ClassMethods
-        def __actions
-          {}
-        end
         def __before_action_name
           :root
         end
@@ -20,7 +17,7 @@ module RSpec
             context next_node[:action][:description] do
               before(&next_node[:action][:block])
               next_node[:examples].each do |example_info|
-                example(example_info[:description], &example_info[:block])
+                example("check:#{example_info[:description]}", &example_info[:block])
               end
               context_proc = create_from_dag(next_node)
               self.module_exec(&context_proc) if context_proc
@@ -31,14 +28,51 @@ module RSpec
 
         def actions(description, &actions_block)
           context description do
+            before_action_name = __before_action_name
             self.module_exec(&actions_block)
             if __action_dags.empty?
               pending 'actions is empty'
               return
             end
-            node = __action_dags[:root]
+            node = __action_dags[before_action_name]
 
             self.module_exec(&create_from_dag(node))
+          end
+        end
+
+        def branch(description, &branch_block)
+          name = "#{description.gsub(/ /, "_")}_#{__action_dags.size}".to_sym
+          before_action_name = __before_action_name
+
+          _action_dags = __action_dags
+          _action_dags[name] = {
+            forwards: [],
+            backwards: [],
+            examples: [],
+          } if _action_dags[name].nil?
+          _action_dags[before_action_name] = {
+            forwards: [],
+            backwards: [],
+            examples: [],
+          } if _action_dags[before_action_name].nil?
+          _action_dags[name].merge!({
+            forwards: _action_dags[name][:forwards],
+            backwards: _action_dags[name][:backwards] | [before_action_name],
+            action: {description: "branch:#{description}", block: Proc.new {} },
+          })
+          _action_dags[before_action_name].merge!({
+            forwards: _action_dags[before_action_name][:forwards] | [name],
+            backwards: _action_dags[before_action_name][:backwards],
+          })
+          idempotently_define_singleton_method(:__action_dags) do ||
+            _action_dags
+          end
+
+          context do
+            idempotently_define_singleton_method(:__before_action_name) do ||
+              name
+            end
+            self.module_exec(&branch_block)
           end
         end
 
@@ -46,11 +80,6 @@ module RSpec
           before_action_name = (not before_action_name.nil?) ? before_action_name : __before_action_name
           name = name || "#{description.gsub(/ /, "_")}_#{__action_dags.size}".to_sym
 
-          _actions = __actions
-          idempotently_define_singleton_method(:__actions) do ||
-            _actions[name] = context
-            _actions
-          end
 
           idempotently_define_singleton_method(:__before_action_name) do ||
             name
@@ -70,7 +99,7 @@ module RSpec
           _action_dags[name].merge!({
             forwards: _action_dags[name][:forwards],
             backwards: _action_dags[name][:backwards] | [before_action_name],
-            action: {description: description, block: action_block},
+            action: {description: "action:#{description}", block: action_block},
           })
           _action_dags[before_action_name].merge!({
             forwards: _action_dags[before_action_name][:forwards] | [name],
--- spec/rspec/core/action_check_helpers_spec.rb
+++ spec/rspec/core/action_check_helpers_spec.rb
@@ -6,9 +6,10 @@ module RSpec::Core
     before do
       @nadeko_is = nil
       @rikka_is = nil
+      @noe_is = nil
     end
 
-=begin
+    
     actions 'first action' do
       action 'set nadeko is cute' do
         @nadeko_is = 'cute'
@@ -30,7 +31,6 @@ module RSpec::Core
 
     actions 'empty actions' do
     end
-=end
 
     actions 'branch' do
       action 'set nadeko is cute' do
@@ -50,6 +50,25 @@ module RSpec::Core
           expect(@nadeko_is).to eq 'cute'
           expect(@rikka_is).to eq 'cute'
         end
+
+        branch 'nest1 nest1' do
+          action 'set noe is cute' do
+            @noe_is = 'cute'
+          end
+
+          check 'nadeko_is = "cute" and rikka_is = "cute" and noe_is = "cute"' do
+            expect(@nadeko_is).to eq 'cute'
+            expect(@rikka_is).to eq 'cute'
+            expect(@noe_is).to eq 'cute'
+          end
+        end
+        branch 'nest1 nest2' do
+          check 'nadeko_is = "cute" and rikka_is = "cute" and noe_is is nil' do
+            expect(@nadeko_is).to eq 'cute'
+            expect(@rikka_is).to eq 'cute'
+            expect(@noe_is).to be_nil
+          end
+        end
       end
 
       branch 'nest2' do
@@ -60,7 +79,6 @@ module RSpec::Core
       end
     end
 
-=begin
     actions 'only context' do
       action 'set rikka is cute' do
         @rikka_is = 'cute'
@@ -93,6 +111,5 @@ module RSpec::Core
       end
 
     end
-=end
   end
 end
